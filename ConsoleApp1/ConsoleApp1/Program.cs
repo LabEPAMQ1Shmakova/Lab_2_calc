0ac889d38ecb62511a820e9b362b3a07e3db527a
YuliyaShmakova <37350313+YuliyaShmakova@users.noreply.github.com>
2018-03-24 00:08:59 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            Console.WriteLine("Hello");
+            Console.ReadKey();
         }
     }
 }
